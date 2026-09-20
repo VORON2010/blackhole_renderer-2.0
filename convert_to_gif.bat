fc3379fddb9afe59f60de76a60647d97dbeed001
Vlad <vlad@example.com>
2026-09-20 11:34:52 +0500

    Fix GIF conversion for older ffmpeg versions

--- convert_to_gif.bat
+++ convert_to_gif.bat
@@ -7,7 +7,7 @@ mkdir media 2>nul
 set count=1
 for %%f in (video\*.mp4) do (
     echo Converting %%f to media\gif!count!.gif
-    ffmpeg -y -i "%%f" -vf "fps=15,scale=600:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 "media\gif!count!.gif"
+    ffmpeg -y -i "%%f" -vf "fps=15,scale=600:-1" "media\gif!count!.gif"
     set /a count+=1
 )
 
